627e17be4fda875ef94cf46ec3675763d0bcd7ab
alejandro.ruiz <alejandro.ruiz@cabify.com>
2021-09-03 08:58:29 +0200

    use webhook

--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const packageInfo = require('./package.json');
+import packageInfo from '../package.json';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import TelegramBot from 'node-telegram-bot-api';
+
+const { BOT_TOKEN } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,14 +13,14 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, '0.0.0.0', () => {
+const server = app.listen(process.env.PORT, '0.0.0.0', () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Web server started at http://%s:%s', host, port);
 });
 
-module.exports = (bot) => {
-  app.post('/' + bot.token, (req, res) => {
+export const web = (bot: TelegramBot): void => {
+  app.post('/' + BOT_TOKEN, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
   });
